5bd6ed36d7d3710fa5a568c51adb27bc5ed39e79
Denis McMahon <denismcmahon@gmail.com>
2025-06-12 21:45:29 +0100

    experience component built

---
+++ src/components/Experience.tsx
@@ -0,0 +1,16 @@
+const Experience = () => {
+    return (
+        <section>
+            <h2>Experience</h2>
+            <div>
+                <h3>Senior Developer – LRN</h3>
+                <p>2022 – Present</p>
+                <p>
+                    Built robust internal tools, optimized content systems, and contributed to Solas Dev Studio side projects.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default Experience;

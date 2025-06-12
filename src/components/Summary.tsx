9f33f76009e388162434b53087099579f3d97663
Denis McMahon <denismcmahon@gmail.com>
2025-06-12 21:43:48 +0100

    summary component built

---
+++ src/components/Summary.tsx
@@ -0,0 +1,12 @@
+const Summary = () => {
+    return (
+        <section>
+            <h2>Summary</h2>
+            <p>
+                Passionate and experienced full-stack JavaScript developer with 20+ years of experience building scalable, high-quality applications...
+            </p>
+        </section>
+    );
+};
+
+export default Summary;

39493daf6f6b5f1d6c7673ab125feea496537fd5
Denis McMahon <denismcmahon@gmail.com>
2025-06-12 21:42:28 +0100

    header component built

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Denis McMahon</h1>
+            <p>Senior Full Stack Developer</p>
+            <p>Email: denismcmahon@gmail.com | LinkedIn: https://www.linkedin.com/in/dmcm82 | GitHub: https://github.com/denismcmahon</p>
+        </header>
+    );
+};
+
+export default Header;

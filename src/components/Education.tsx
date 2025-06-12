000ae3ca40c381d32bbbcbae0734025702e3c384
Denis McMahon <denismcmahon@gmail.com>
2025-06-12 22:12:39 +0100

    education component built

---
+++ src/components/Education.tsx
@@ -0,0 +1,12 @@
+const Education = () => {
+  return (
+    <section>
+      <h2>Education</h2>
+      <p>
+        Self-taught developer with 20+ years industry experience. Started coding at 18 under mentorship from father’s computer business.
+      </p>
+    </section>
+  );
+};
+
+export default Education;
